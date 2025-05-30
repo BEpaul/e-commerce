c70a6093cf7c9e392c10cbe4ce77bc87cabd5c01
Minhyuk Kim <rhrlwld@gmail.com>
2025-05-30 09:46:43 +0900

    [CHORE] Swagger UI 적용

--- src/main/java/kr/hhplus/be/server/domain/point/dto/request/PointChargeRequest.java
+++ src/main/java/kr/hhplus/be/server/domain/point/dto/request/PointChargeRequest.java
@@ -1,12 +1,17 @@
 package kr.hhplus.be.server.domain.point.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
+@Schema(description = "포인트 충전 요청 DTO")
 @Getter
 public class PointChargeRequest {
 
+    @Schema(description = "사용자 ID", example = "1")
     private Long userId;
+
+    @Schema(description = "충전할 포인트 금액", example = "5000")
     private Long amount;
 
     @Builder
