9b9273b66508df9380ed4f48aeb9159bc3855aa8
Minhyuk Kim <rhrlwld@gmail.com>
2025-06-04 21:04:17 +0900

    [FEAT] 상품 엔티티, 레포지토리 구현 및 BaseTimeEntity 추가

---
+++ src/main/java/kr/hhplus/be/server/domain/product/entity/Product.java
@@ -0,0 +1,38 @@
+package kr.hhplus.be.server.domain.product.entity;
+
+import jakarta.persistence.*;
+import kr.hhplus.be.server.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Column(nullable = false)
+    private Long stock;
+
+    @Column(nullable = false, length = 300)
+    private String description;
+
+    @Builder
+    public Product(String title, Long price, Long stock, String description) {
+        this.title = title;
+        this.price = price;
+        this.stock = stock;
+        this.description = description;
+    }
+}
