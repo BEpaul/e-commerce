410665983d344842578314f7e6c84196551b6a35
Minhyuk Kim <rhrlwld@gmail.com>
2025-06-06 02:36:53 +0900

    [FEAT] 포인트 조회 및 충전 컨트롤러 구현

--- src/main/java/kr/hhplus/be/server/domain/point/controller/PointController.java
+++ src/main/java/kr/hhplus/be/server/domain/point/controller/PointController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import kr.hhplus.be.server.common.response.ApiResponse;
 import kr.hhplus.be.server.domain.point.dto.request.PointChargeRequest;
 import kr.hhplus.be.server.domain.point.dto.response.PointResponse;
+import kr.hhplus.be.server.domain.point.service.PointService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,12 +13,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/points")
 public class PointController {
 
+    private final PointService pointService;
+
     @Operation(summary = "포인트 조회", description = "사용자의 포인트를 조회합니다.")
     @GetMapping("/{userId}")
     public ApiResponse<PointResponse> getPoints(@PathVariable Long userId) {
-        PointResponse pointResponse = PointResponse.builder()
-                .point(10000L)
-                .build();
+        PointResponse pointResponse = pointService.getPoint(userId);
 
         return ApiResponse.success(pointResponse, "포인트 조회 성공");
     }
@@ -25,11 +26,7 @@ public class PointController {
     @Operation(summary = "포인트 충전", description = "사용자의 포인트를 충전합니다.")
     @PostMapping("/charge")
     public ApiResponse<PointResponse> chargePoints(@RequestBody PointChargeRequest pointChargeRequest) {
-        long chargedPoints = 5000 + pointChargeRequest.getAmount();
-
-        PointResponse response = PointResponse.builder()
-                .point(chargedPoints)
-                .build();
+        PointResponse response = pointService.chargePoint(pointChargeRequest.getUserId(), pointChargeRequest.getAmount());
 
         return ApiResponse.success(response, "포인트 충전 성공");
     }
