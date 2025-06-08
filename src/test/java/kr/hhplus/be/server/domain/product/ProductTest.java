28d204617206d82b829dc8ac8d4332b8cedc77cc
Minhyuk Kim <rhrlwld@gmail.com>
2025-06-08 23:19:09 +0900

    [TEST] 상품 도메인 재고 차감 테스트 추가

---
+++ src/test/java/kr/hhplus/be/server/domain/product/ProductTest.java
@@ -0,0 +1,57 @@
+package kr.hhplus.be.server.domain.product;
+
+import kr.hhplus.be.server.common.exception.OutOfStockException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@ExtendWith(MockitoExtension.class)
+class ProductTest {
+
+    @Test
+    @DisplayName("정상적으로 상품 재고가 차감된다")
+    void 정상적으로_상품_재고가_차감된다() {
+        // given
+        Long stock = 10L;
+        Long orderQuantity = 5L;
+        Long expectedStock = stock - orderQuantity;
+
+        Product product = Product.builder()
+                .name("상품 A")
+                .price(10000L)
+                .stock(stock)
+                .description("상품 A 설명")
+                .build();
+
+        // when
+        product.decreaseStock(orderQuantity);
+
+        // then
+        assertThat(product.getStock()).isEqualTo(expectedStock);
+    }
+
+    @Test
+    @DisplayName("주문 수량보다 상품 재고가 부족할 경우 예외가 발생한다")
+    void 주문_수량보다_상품_재고가_부족할_경우_예외가_발생한다() {
+        // given
+        Long stock = 5L;
+        Long orderQuantity = 10L;
+
+        Product product = Product.builder()
+                .name("상품 A")
+                .price(10000L)
+                .stock(stock)
+                .description("상품 A 설명")
+                .build();
+
+        // when & then
+        assertThatThrownBy(() -> product.decreaseStock(orderQuantity))
+                .isInstanceOf(OutOfStockException.class);
+
+        assertThat(product.getStock()).isEqualTo(stock);
+    }
+}
