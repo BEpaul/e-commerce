ddb0a4ac76f67067badabab74399fdf402eec722
Minhyuk Kim <rhrlwld@gmail.com>
2025-06-08 21:13:41 +0900

    [FEAT] 주문 생성 서비스 구현

---
+++ src/main/java/kr/hhplus/be/server/domain/order/OrderProductRepository.java
@@ -0,0 +1,5 @@
+package kr.hhplus.be.server.domain.order;
+
+public interface OrderProductRepository {
+    void save(OrderProduct orderProduct);
+}
---
+++ src/main/java/kr/hhplus/be/server/domain/order/OrderStatus.java
@@ -0,0 +1,7 @@
+package kr.hhplus.be.server.domain.order;
+
+public enum OrderStatus {
+    DONE, // 주문 완료
+    CANCELED, // 주문 취소
+    WAITING, // 주문 대기 중
+}
