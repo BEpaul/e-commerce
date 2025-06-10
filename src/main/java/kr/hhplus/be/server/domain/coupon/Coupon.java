2468d4c021d8d5358edaed34cd1a94a997d45e26
Minhyuk Kim <rhrlwld@gmail.com>
2025-06-11 01:35:39 +0900

    [FEAT] 쿠폰 레포지토리 구현

--- src/main/java/kr/hhplus/be/server/domain/coupon/Coupon.java
+++ src/main/java/kr/hhplus/be/server/domain/coupon/Coupon.java
@@ -1,17 +1,26 @@
 package kr.hhplus.be.server.domain.coupon;
 
+import jakarta.persistence.*;
 import kr.hhplus.be.server.common.exception.NotSupportedDiscountTypeException;
-import kr.hhplus.be.server.common.exception.OutOfStockCouponException;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Coupon {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "coupon_id")
     private Long id;
     private Long discountValue;
+
+    @Enumerated(EnumType.STRING)
     private DiscountType discountType;
     private String title;
     private Long stock;
