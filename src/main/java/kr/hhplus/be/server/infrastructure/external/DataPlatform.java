f0504691c68ab3d234a575ca92aa68be4923f095
Minhyuk Kim <rhrlwld@gmail.com>
2025-06-10 20:40:26 +0900

    [FEAT] 결제 도메인 및 Mock 외부 데이터 플랫폼 생성

---
+++ src/main/java/kr/hhplus/be/server/infrastructure/external/DataPlatform.java
@@ -0,0 +1,10 @@
+package kr.hhplus.be.server.infrastructure.external;
+
+import kr.hhplus.be.server.domain.payment.Payment;
+
+public class DataPlatform {
+
+    public static boolean sendData(Payment payment) {
+        return true;
+    }
+}
