6cc2a165ed2933e4220790fa562885e6ae7c5aee
Minhyuk Kim <rhrlwld@gmail.com>
2025-06-16 20:54:11 +0900

    [FEAT] 판매량 상위 상품 레포지토리 JPA 메서드 추가

--- src/main/java/kr/hhplus/be/server/infrastructure/persistence/bestseller/BestSellerRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/persistence/bestseller/BestSellerRepository.java
@@ -4,6 +4,10 @@ import kr.hhplus.be.server.domain.bestseller.BestSeller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface BestSellerRepository extends JpaRepository<BestSeller, Long> {
+    List<BestSeller> findByTopDateOrderByRankAsc(LocalDateTime topDate);
 }
