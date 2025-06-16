f2ee912f8b977a315037e7b6c2a90d3a516dc6d6
Minhyuk Kim <rhrlwld@gmail.com>
2025-06-16 21:12:49 +0900

    [FEAT] 판매량 상위 상품 조회 서비스 구현

---
+++ src/main/java/kr/hhplus/be/server/application/bestseller/BestSellerService.java
@@ -0,0 +1,29 @@
+package kr.hhplus.be.server.application.bestseller;
+
+import kr.hhplus.be.server.common.exception.BestSellerNotFoundException;
+import kr.hhplus.be.server.domain.bestseller.BestSeller;
+import kr.hhplus.be.server.infrastructure.persistence.bestseller.BestSellerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BestSellerService {
+
+    private final BestSellerRepository bestSellerRepository;
+
+    @Transactional(readOnly = true)
+    public List<BestSeller> getTopProducts(LocalDateTime date) {
+        List<BestSeller> bestSellers = bestSellerRepository.findByTopDateOrderByRankAsc(date);
+        
+        if (bestSellers.isEmpty()) {
+            throw new BestSellerNotFoundException("해당 날짜의 베스트셀러 데이터가 존재하지 않습니다.");
+        }
+        
+        return bestSellers;
+    }
+}
---
+++ src/main/java/kr/hhplus/be/server/common/exception/BestSellerNotFoundException.java
@@ -0,0 +1,7 @@
+package kr.hhplus.be.server.common.exception;
+
+public class BestSellerNotFoundException extends RuntimeException {
+    public BestSellerNotFoundException(String message) {
+        super(message);
+    }
+}
