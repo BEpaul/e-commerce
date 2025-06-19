04370ea3b1277530fc739be719420f293cce9c58
Minhyuk Kim <rhrlwld@gmail.com>
2025-06-19 16:14:30 +0900

    [CHORE] 쿠폰 발급 동시성 처리 재시도 로직 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation ("org.springframework.boot:spring-boot-starter-aop")
+	implementation ("org.springframework.retry:spring-retry")
 
     // DB
 	runtimeOnly("com.mysql:mysql-connector-j")
