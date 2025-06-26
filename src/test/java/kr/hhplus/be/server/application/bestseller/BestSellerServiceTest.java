cd9d9329476b6e4e81f2684b55c8d818731978e5
Minhyuk Kim <rhrlwld@gmail.com>
2025-06-27 01:35:04 +0900

    [TEST] 베스트셀러 조회 API 캐싱 적용에 따른 TC 추가 및 수정

--- src/test/java/kr/hhplus/be/server/application/bestseller/BestSellerServiceTest.java
+++ src/test/java/kr/hhplus/be/server/application/bestseller/BestSellerServiceTest.java
@@ -11,25 +11,29 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static kr.hhplus.be.server.common.exception.ErrorCode.BESTSELLER_NOT_FOUND;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.lenient;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class BestSellerServiceTest {
-    private static final Logger log = LoggerFactory.getLogger(BestSellerServiceTest.class);
+
     @InjectMocks
     private BestSellerService bestSellerService;
 
     @Mock
     private BestSellerRepository bestSellerRepository;
+    
+    @Mock
+    private BestSellerCacheService bestSellerCacheService;
 
     private List<BestSeller> createBestSellers(LocalDateTime topDate) {
         return Arrays.asList(
@@ -79,48 +83,88 @@ class BestSellerServiceTest {
     }
 
     @Test
-    void 오늘의_상위상품_5개를_조회한다() {
+    void 캐시에_데이터가_있으면_캐시에서_조회한다() {
         // given
         LocalDateTime today = LocalDateTime.now();
-        List<BestSeller> expectedBestSellers = createBestSellers(today);
-
-        lenient().when(bestSellerRepository.findByTopDateOrderByRankingAsc(today))
-                .thenReturn(expectedBestSellers);
+        LocalDate todayDate = today.toLocalDate();
+        List<BestSeller> cachedBestSellers = createBestSellers(today);
+        
+        given(bestSellerCacheService.getCachedBestSellers(todayDate))
+                .willReturn(cachedBestSellers);
 
         // when
         List<BestSeller> actualBestSellers = bestSellerService.getTopProducts(today);
 
         // then
         assertBestSellers(actualBestSellers);
+        then(bestSellerCacheService).should().getCachedBestSellers(todayDate);
+        then(bestSellerRepository).should(never()).findByTopDateOrderByRankingAsc(any());
+        then(bestSellerCacheService).should(never()).cacheBestSellers(any(), any());
+    }
+
+    @Test
+    void 캐시에_데이터가_없으면_DB에서_조회하고_캐시에_저장한다() {
+        // given
+        LocalDateTime today = LocalDateTime.now();
+        LocalDate todayDate = today.toLocalDate();
+        List<BestSeller> expectedBestSellers = createBestSellers(today);
+        
+        given(bestSellerCacheService.getCachedBestSellers(todayDate))
+                .willReturn(null);
+        given(bestSellerRepository.findByTopDateOrderByRankingAsc(today))
+                .willReturn(expectedBestSellers);
+
+        // when
+        List<BestSeller> actualBestSellers = bestSellerService.getTopProducts(today);
+
+        // then
+        assertBestSellers(actualBestSellers);
+        then(bestSellerCacheService).should().getCachedBestSellers(todayDate);
+        then(bestSellerRepository).should().findByTopDateOrderByRankingAsc(today);
+        then(bestSellerCacheService).should().cacheBestSellers(todayDate, expectedBestSellers);
     }
 
     @Test
     void 시간이_다른_같은_날짜의_상품을_조회한다() {
         // given
         LocalDateTime searchTime = LocalDateTime.of(2024, 3, 18, 15, 30, 0);
+        LocalDate searchDate = searchTime.toLocalDate();
         LocalDateTime storedTime = LocalDateTime.of(2024, 3, 18, 0, 0, 0);
         List<BestSeller> expectedBestSellers = createBestSellers(storedTime);
-
-        lenient().when(bestSellerRepository.findByTopDateOrderByRankingAsc(searchTime))
-            .thenReturn(expectedBestSellers);
+        
+        given(bestSellerCacheService.getCachedBestSellers(searchDate))
+                .willReturn(null);
+        given(bestSellerRepository.findByTopDateOrderByRankingAsc(searchTime))
+            .willReturn(expectedBestSellers);
 
         // when
         List<BestSeller> actualBestSellers = bestSellerService.getTopProducts(searchTime);
 
         // then
         assertBestSellers(actualBestSellers);
+        then(bestSellerCacheService).should().getCachedBestSellers(searchDate);
+        then(bestSellerRepository).should().findByTopDateOrderByRankingAsc(searchTime);
+        then(bestSellerCacheService).should().cacheBestSellers(searchDate, expectedBestSellers);
     }
 
     @Test
     void 존재하지_않는_날짜의_상품을_조회하면_예외가_발생한다() {
         // given
         LocalDateTime nonExistentDate = LocalDateTime.now().plusDays(1);
-        lenient().when(bestSellerRepository.findByTopDateOrderByRankingAsc(nonExistentDate))
-                .thenReturn(Collections.emptyList());
+        LocalDate nonExistentDateOnly = nonExistentDate.toLocalDate();
+        
+        given(bestSellerCacheService.getCachedBestSellers(nonExistentDateOnly))
+                .willReturn(null);
+        given(bestSellerRepository.findByTopDateOrderByRankingAsc(nonExistentDate))
+                .willReturn(Collections.emptyList());
 
         // when & then
         assertThatThrownBy(() -> bestSellerService.getTopProducts(nonExistentDate))
                 .isInstanceOf(ApiException.class)
                 .hasMessage(BESTSELLER_NOT_FOUND.getMessage());
+        
+        then(bestSellerCacheService).should().getCachedBestSellers(nonExistentDateOnly);
+        then(bestSellerRepository).should().findByTopDateOrderByRankingAsc(nonExistentDate);
+        then(bestSellerCacheService).should(never()).cacheBestSellers(any(), any());
     }
 }
