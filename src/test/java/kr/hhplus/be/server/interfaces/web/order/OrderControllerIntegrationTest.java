64a5237174da7df37c0865463ba6fb538d974f69
Minhyuk Kim <rhrlwld@gmail.com>
2025-07-05 09:44:42 +0900

    [RENAME] 외부 데이터 플랫폼 디렉토리 이동

--- src/test/java/kr/hhplus/be/server/interfaces/web/order/OrderControllerIntegrationTest.java
+++ src/test/java/kr/hhplus/be/server/interfaces/web/order/OrderControllerIntegrationTest.java
@@ -7,7 +7,7 @@ import kr.hhplus.be.server.domain.order.Order;
 import kr.hhplus.be.server.domain.order.OrderRepository;
 import kr.hhplus.be.server.domain.point.Point;
 import kr.hhplus.be.server.domain.product.Product;
-import kr.hhplus.be.server.infrastructure.external.DataPlatform;
+import kr.hhplus.be.server.infrastructure.external.payment.DataPlatform;
 import kr.hhplus.be.server.infrastructure.persistence.point.PointRepository;
 import kr.hhplus.be.server.infrastructure.persistence.product.ProductRepository;
 import kr.hhplus.be.server.interfaces.web.order.dto.request.OrderProductRequest;
