5b5eb7677a9096b150d0fe9e1c50a14f2fc1fade
Minhyuk Kim <rhrlwld@gmail.com>
2025-07-21 18:42:15 +0900

    [FEAT] 실시간 주문 정보 전송 실패 시 DLQ로 전송하는 로직 추가

---
+++ src/main/java/kr/hhplus/be/server/domain/order/event/dto/OrderCompletedDlqEventDto.java
@@ -0,0 +1,27 @@
+package kr.hhplus.be.server.domain.order.event.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderCompletedDlqEventDto {
+    
+    private OrderCompletedEventDto originalEvent;
+    private String failureReason;
+    private LocalDateTime failedAt;
+    private int retryCount;
+
+    private static final int MAX_RETRY_COUNT = 3;
+    
+    public boolean canRetry() {
+        return retryCount < MAX_RETRY_COUNT;
+    }
+    
+    public void incrementRetryCount() {
+        this.retryCount++;
+    }
+} 
